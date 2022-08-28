8dd634362d2fa96dff4e31f0fc37ee2bc2afb1ee
Eduard Karol <korol94.94@gmail.com>
2022-08-29 00:24:55 +0300

    trying a new design for tkinter frame, and elements

--- checker_steam_with_GUI.py
+++ checker_steam_with_GUI.py
@@ -12,6 +12,9 @@ from tkinter import ttk
 from multiprocessing import Process
 
 root = Tk()
+style = ttk.Style(root)
+root.tk.call('source', 'awthemes/awdark.tcl')
+style.theme_use('awdark')
 
 th = None
 stop_process = False
@@ -19,7 +22,7 @@ root.title('SteamInvestChecker')
 
 
 def generate_frame():
-    frame_name = Frame(root)
+    frame_name = ttk.Frame(root)
     frame_name.grid(row=0, column=0, sticky='news')
     return frame_name
 
@@ -30,14 +33,14 @@ data_menu.tkraise()
 
 def generate_elements(data_menu, text_: str, font_name: str = 'Arial', font_size: int = 14, row_: int = 0,
                       column_: int = 0, sticky_: str = 'ns'):
-    element_name = Label(data_menu, text=text_, font=(font_name, font_size))
+    element_name = ttk.Label(data_menu, text=text_)
     element_name.grid(row=row_, column=column_, sticky=sticky_)
 
 
-def generate_entry(data_menu, text_entry: str, width_: int = 6, font_name: str = 'Arial', font_size: int = 14,
+def generate_entry(data_menu, text_entry: str, width_: int = 9, font_name: str = 'Arial', font_size: int = 14,
                    row_: int = 1,
-                   column_: int = 0):
-    entry_name = Entry(data_menu, width=width_, text=text_entry, font=(font_name, font_size))
+                   column_: int = 0, style_entry: str = 'Entry'):
+    entry_name = ttk.Entry(data_menu, width=width_, text=text_entry)
     entry_name.grid(row=row_, column=column_)
     return entry_name
 
@@ -48,7 +51,7 @@ def generate_button(text_btn: str, width_btn: int = 2, height_btn: int = 1, bg_b
                     column_btn: int = 0,
                     columnspan_btn: int = 3, sticky_btn: str = 'ne', padx_btn: int = 0, pady_btn: int = 0,
                     ipadx_btn: int = 13, ipady_btn: int = 0):
-    button_name = Button(text=text_btn, width=width_btn, height=height_btn, bg=bg_btn, font=(font_name, font_size),
+    button_name = ttk.Button(text=text_btn, width=width_btn,
                          state=state_btn, command=command_btn)
     button_name.grid(row=row_btn, column=column_btn, columnspan=columnspan_btn, sticky=sticky_btn, padx=padx_btn,
                      pady=pady_btn, ipadx=ipadx_btn, ipady=ipady_btn)
@@ -65,15 +68,12 @@ def count_lines():
 generate_elements(data_menu, text_='Item')
 name_entry = generate_entry(data_menu, text_entry='name', row_=1)
 generate_elements(data_menu, text_='Url', column_=1)
-url_entry = generate_entry(data_menu, width_=20, text_entry='Url', row_=1, column_=1)
+url_entry = generate_entry(data_menu, width_=32, text_entry='Url', row_=1, column_=1)
 generate_elements(data_menu, text_='Amount', column_=2)
 amount_entry = generate_entry(data_menu, text_entry='Amount', row_=1, column_=2)
-text_process = scrolledtext.ScrolledText(data_menu, width=43, height=12)
+text_process = scrolledtext.ScrolledText(data_menu, width=40, height=12)
 text_process.grid(row=2, column=0, columnspan=3)
-style = ttk.Style()
-style.theme_use('default')
-style.configure("black.Horizontal.TProgressbar", background='#5882FA')
-progress_bar = Progressbar(data_menu, length=235, style='black.Horizontal.TProgressbar')
+progress_bar = Progressbar(data_menu, length=235, style='Horizontal.TProgressbar')
 progress_bar['value'] = 0
 progress_bar.grid(row=3, column=0, columnspan=3, padx=3, pady=0, ipadx=12, ipady=0)
 
@@ -116,11 +116,11 @@ def stop():
     global stop_process, th
     stop_process = True
     th.join()
-    generate_button(text_btn='Add item', width_btn=9, bg_btn='#FE9A2E', sticky_btn='nw', padx_btn=0,
+    generate_button(text_btn='Add item', width_btn=11, bg_btn='#FE9A2E', sticky_btn='nw', padx_btn=0,
                     pady_btn=0,
                     ipadx_btn=0, ipady_btn=0, command_btn=add_item)
     generate_button(text_btn=' Load', width_btn=3, padx_btn=53, ipadx_btn=10, command_btn=load)
-    generate_button(text_btn='Calculate', width_btn=10, command_btn=multi_calculate, padx_btn=91, ipadx_btn=12,
+    generate_button(text_btn='Calculate', width_btn=11, command_btn=multi_calculate, padx_btn=77, ipadx_btn=12,
                     sticky_btn='nw')
     generate_button(text_btn='Save', command_btn=save)
     name_entry.config(state='normal')
@@ -154,13 +154,13 @@ headers = {
 }
 cookie = {'steamLoginSecure': '12345'}
 
-generate_button(text_btn='Add item', width_btn=9, bg_btn='#FE9A2E', sticky_btn='nw', padx_btn=0, pady_btn=0,
+generate_button(text_btn='Add item', width_btn=11, bg_btn='#FE9A2E', sticky_btn='nw', padx_btn=0, pady_btn=0,
                 ipadx_btn=0, ipady_btn=0, command_btn=add_item)
 generate_button(text_btn=' Load', width_btn=3, padx_btn=53, ipadx_btn=10, command_btn=load)
-calc = generate_button(text_btn='Calculate', width_btn=10, command_btn=multi_calculate, padx_btn=91, ipadx_btn=12,
+generate_button(text_btn='Calculate', width_btn=11, command_btn=multi_calculate, padx_btn=77, ipadx_btn=12,
                        sticky_btn='nw')
 generate_button(text_btn='Save', command_btn=save)
-generate_button(text_btn='✕', width_btn=1, bg_btn='#FE2E2E', padx_btn=105, pady_btn=0, command_btn=stop_threading)
+generate_button(text_btn='✕', width_btn=2, bg_btn='#FE2E2E', padx_btn=105, pady_btn=0, command_btn=stop_threading)
 
 
 def check_price(url: str, count: int):
@@ -177,12 +177,12 @@ def check_price(url: str, count: int):
     text_process.insert(INSERT, '\n'f'{name_ + " " + str(current_case) + "$"}')
     count_li = count_lines()
     progress_bar['value'] += 100 / count_li
-    generate_button(text_btn='Add item', width_btn=9, bg_btn='#DCDCDC', state_btn='disabled',
+    generate_button(text_btn='Add item', width_btn=11, bg_btn='#DCDCDC', state_btn='disabled',
                     sticky_btn='nw', padx_btn=0,
                     pady_btn=0,
                     ipadx_btn=0, ipady_btn=0, command_btn=add_item)
-    generate_button(text_btn='Calculate', width_btn=10, bg_btn='#DCDCDC', state_btn='disabled',
-                    command_btn=multi_calculate, padx_btn=91, ipadx_btn=12,
+    generate_button(text_btn='Calculate', width_btn=11, bg_btn='#DCDCDC', state_btn='disabled',
+                    command_btn=multi_calculate, padx_btn=77, ipadx_btn=12,
                     sticky_btn='nw')
     generate_button(text_btn='Save', command_btn=save, bg_btn='#DCDCDC', state_btn='disabled')
     generate_button(text_btn=' Load', width_btn=3, bg_btn='#DCDCDC', state_btn='disabled', padx_btn=53, ipadx_btn=10,
